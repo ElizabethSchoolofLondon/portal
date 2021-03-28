e101c75b5c96481b23c420e35951bb15c7863520
Rafin Rahman <ra-fin@live.com>
2021-03-28 15:12:36 +0100

    token expiry changed to 10h

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -61,7 +61,7 @@ router.post(
       jwt.sign(
         payload,
         config.get('jwtSecret'),
-        { expiresIn: 604800 },
+        { expiresIn: 36000 },
         (err, token) => {
           if (err) throw err
           res.json({ token })
